e5091c3a70dd861a8847220799ac8572f8cc88db
Pavel Shustov <p.shustov@gmail.com>
2020-09-25 06:20:07 +0300

    equation to real

--- FourierCudaSolverAxion/Include/equations.h
+++ FourierCudaSolverAxion/Include/equations.h
@@ -6,7 +6,7 @@ public:
 	equationsAxionSymplectic_3D(cudaGrid_3D& _Grid);
 	~equationsAxionSymplectic_3D() { cudaStreamDestroy(stream); }
 
-	void equationCuda(const double dt);
+	void equationCuda(const real dt);
 	void getNonlin_Phi4_Phi6();
 
 	void setCudaStream(cudaStream_t& _stream) { stream = _stream; }
