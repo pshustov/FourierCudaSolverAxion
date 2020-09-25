2a1ddcffab89757a76669d88716ad78d03e45a00
Pavel Shustov <p.shustov@gmail.com>
2020-09-25 06:17:57 +0300

    cudaGrid to real Grid

--- FourierCudaSolverAxion/Include/cudaGrid.h
+++ FourierCudaSolverAxion/Include/cudaGrid.h
@@ -3,7 +3,7 @@
 #include "cudaFFT.h"
 
 const int N_MIN = 32;
-const double Ma_PI = 3.1415926535897932384626433832795;
+const real Ma_PI = 3.1415926535897932384626433832795;
 const int BLOCK_SIZE = 128;
 
 class cudaGrid_3D
@@ -31,10 +31,10 @@ public:
 	size_t getN2buf() const { return N2buf; }
 	size_t getN3buf() const { return N3buf; }
 
-	double getL1() const { return L1; }
-	double getL2() const { return L2; }
-	double getL3() const { return L3; }
-	double getVolume() const { return L1 * L2 * L3; }
+	real getL1() const { return L1; }
+	real getL2() const { return L2; }
+	real getL3() const { return L3; }
+	real getVolume() const { return L1 * L2 * L3; }
 
 	cudaRVector get_x1() const { return x1; }
 	cudaRVector get_x2() const { return x2; }
@@ -51,9 +51,9 @@ public:
 	cudaCVector3& get_P() { return P; }
 	cudaCVector3& get_T() { return T; }
 
-	double get_time() const { return current_time; }
-	double get_lambda() const { return lambda; }
-	double get_g() const { return g; }
+	real get_time() const { return current_time; }
+	real get_lambda() const { return lambda; }
+	real get_g() const { return g; }
 	cudaStream_t get_mainStream() const { return mainStream; }
 
 	/// FFT and IFFT
@@ -64,8 +64,8 @@ public:
 
 
 	/// Sets 
-	void set_lambda(const double _lambda) { lambda = _lambda; }
-	void set_g(const double _g) { g = _g; }
+	void set_lambda(const real _lambda) { lambda = _lambda; }
+	void set_g(const real _g) { g = _g; }
 	void setSmthChanged() { 
 		isEnergyCalculateted = false; 
 		isIFFTsyncQ = false;
@@ -74,23 +74,23 @@ public:
 	}
 
 	/// Other methods 
-	double get_dt(const double precision) const
+	real get_dt(const real precision) const
 	{
 		return precision;
 	}
 	void set_sizes();
 	void set_xk();
-	void timestep(double dt) { setSmthChanged();  current_time += dt; }
-	double getEnergy();
+	void timestep(real dt) { setSmthChanged();  current_time += dt; }
+	real getEnergy();
 
 	void calculateQPsqr();
-	double getMaxValQsqr();
+	real getMaxValQsqr();
 	
 	void printingVTK(std::ofstream& outVTK);
 
 private:
 	size_t N1, N2, N3, N3red;
-	double L1, L2, L3;
+	real L1, L2, L3;
 
 	cudaRVector x1, x2, x3;
 	cudaRVector3 k1, k2, k3;
@@ -105,11 +105,11 @@ private:
 
 	cuFFT cufft;
 
-	double lambda, g;
-	double current_time;
+	real lambda, g;
+	real current_time;
 	bool isIFFTsyncQ, isIFFTsyncP, isEnergyCalculateted, isQPsqrCalculated;
 
-	double energy;
+	real energy;
 
 	cudaStream_t mainStream, printStream;
 };
